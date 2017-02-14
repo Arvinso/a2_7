10b78459f36ea575085b84fe233e281b5dcdc12f
Arvin <vireshsoedhwa@gmail.com>
2017-02-13 17:57:35 -0800

    printing time works now

--- main.c
+++ main.c
@@ -6,12 +6,40 @@
  */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+#define FILENAME "thefile"
 
 int main()
 {
-	printf("hello");
+	int *fptr;
+	time_t rawtime;
+	struct tm *timeinfo;
 
+	char *buff;
+
+
+	fptr = fopen(FILENAME, "w");
+
+	if(fptr == NULL)
+	{
+		perror("fopen");
+		exit(EXIT_FAILURE);
+		return 1;
+	}
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	buff = asctime(timeinfo);
+
+	printf(buff);
+
+	write(fptr, &buff, sizeof(buff));
 
 	return 0;
 }
